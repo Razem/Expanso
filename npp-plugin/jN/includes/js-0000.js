60c34e8374fd589bf590f5cc497b7f7a03343877
Radomír Žemlička <info@razem.cz>
2013-08-19 11:19:54 +0200

    UTF-8 fix in the npp-plugin

--- npp-plugin/jN/includes/js-0000.js
+++ npp-plugin/jN/includes/js-0000.js
@@ -1,18 +1,23 @@
 var JSTools = {},
-    XFile = {};
+    XFile = {},
+    UTF8 = {};
 (function () {
   var fs = JSTools.fs = new ActiveXObject("Scripting.FileSystemObject");
   
   XFile.read = function (file) {
-    var f = fs.OpenTextFile(file, 1, false, 1);
+    var f = fs.OpenTextFile(file, 1, false, 0);
     var text = f.ReadAll();
     f.Close();
+    
+    text = decodeFrom(65001, text);
     
     return text;
   };
   
   XFile.write = function (file, text) {
-    var f = fs.CreateTextFile(file, true);
+    var f = fs.CreateTextFile(file, true, false);
+    // f.BinaryWrite(utf8.UnicodeToUtf8(text));
+    text = encodeTo(65001, text);
     f.Write(text);
     f.Close();
   };
